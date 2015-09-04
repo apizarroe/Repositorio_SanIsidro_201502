65219d42fa38d7c73282e49fbac20ab54e461999
luisreque <luis.reque86@gmail.com>
2015-09-04 01:20:55 -0500

    commit models

---
+++ Proyecto_Municipalidad_SanIsidro/GAC/Models/ProyectoInversion/CreateProyectoInversionModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GAC.Models.ProyectoInversion
+{
+    public class CreateProyectoInversionModel
+    {
+        [Required]
+        public String Nombre { get; set; }
+        [Required]
+        public String Descripcion { get; set; }
+        [Required]
+        public String Ubicacion { get; set; }
+        [Required]
+        public int IdVia { get; set; }
+        [Required]
+        public String TipoVia { get; set; }
+        [Required]
+        public int Beneficiarios { get; set; }
+        [Required]
+        public Decimal ValorReferencial { get; set; }
+    }
+}
---
+++ Proyecto_Municipalidad_SanIsidro/GAC/Models/ProyectoInversion/UpdateProyectoInversionModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GAC.Models.ProyectoInversion
+{
+    public class UpdateProyectoInversionModel : IValidatableObject
+    {
+        [Required]
+        public int IdProyecto { get; set; }
+        
+        public String CodSNIP { get; set; }
+        [Required]
+        public String Nombre { get; set; }
+        [Required]
+        public String Descripcion { get; set; }
+        [Required]
+        public String Ubicacion { get; set; }
+        [Required]
+        public int IdVia { get; set; }
+        [Required]
+        public String TipoVia { get; set; }
+        [Required]
+        public int Beneficiarios { get; set; }
+        [Required]
+        public Decimal ValorReferencial { get; set; }
+        [Required]
+        public String IdEstado { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.IdEstado!=Dominio.Core.Entities.ProyectoInversion.STR_ID_ESTADO_INVIABLE && String.IsNullOrWhiteSpace(this.IdEstado))
+                yield return new ValidationResult("El campo CodSNIP es obligatorio.");
+        }
+
+        public String SearchCodSNIP { get; set; }
+        public String SearchNombre { get; set; }
+        public String SearchUbicacion { get; set; }
+        public String SearchIdEstado { get; set; }
+    }
+}
