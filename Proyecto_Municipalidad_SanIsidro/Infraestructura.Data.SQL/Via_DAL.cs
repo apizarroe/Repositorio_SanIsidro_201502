871dfd6d3d2b4914df107c01f04f1eec42ed6164
luisreque <luis.reque86@gmail.com>
2015-09-04 01:14:48 -0500

    commit Via_DAL

---
+++ Proyecto_Municipalidad_SanIsidro/Infraestructura.Data.SQL/Via_DAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Core.Entities;
+
+namespace Infraestructura.Data.SQL
+{
+    public class Via_DAL
+    {
+        public List<Via> ObtieneVias(String pStrIdTipo)
+        {
+            List<Via> lstVias = new List<Via>();
+            try
+            {
+                MuniIntegrado objContext = new MuniIntegrado();
+                var lstViasTmp = objContext.MA_VIA.Where(v => v.noTipoVia == pStrIdTipo || pStrIdTipo == null).ToList();
+
+                foreach (MA_VIA objViaTmp in lstViasTmp)
+                {
+                    Via objVia = new Via();
+                    objVia.IdVia = objViaTmp.coVia;
+                    objVia.Nombre = objViaTmp.noNomVia;
+                    objVia.Tipo = objViaTmp.noTipoVia;
+                    lstVias.Add(objVia);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+            return lstVias;
+        }
+    }
+}
